3f58afdea996b834332b582a44be6a803662a284
Michelle Beshears <meeshysu@gmail.com>
2018-11-09 12:50:55 -0600

    all the legos print now working on starting drop down

--- src/data/partsData.js
+++ src/data/partsData.js
@@ -4,7 +4,6 @@ const getAllHeadsFromDb = () => new Promise((resolve, reject) => {
   axios.get('http://localhost:3004/heads')
     .then((data) => {
       const cleanData = data.data[0];
-      console.log(cleanData);
       resolve(cleanData);
     })
     .catch((error) => {
@@ -12,4 +11,26 @@ const getAllHeadsFromDb = () => new Promise((resolve, reject) => {
     });
 });
 
-export default getAllHeadsFromDb;
+const getAllTorsosFromDb = () => new Promise((resolve, reject) => {
+  axios.get('http://localhost:3004/torsos')
+    .then((data) => {
+      const cleanData = data.data[0];
+      resolve(cleanData);
+    })
+    .catch((error) => {
+      reject(error);
+    });
+});
+
+const getAllLegsFromDb = () => new Promise((resolve, reject) => {
+  axios.get('http://localhost:3004/legs')
+    .then((data) => {
+      const cleanData = data.data[0];
+      resolve(cleanData);
+    })
+    .catch((error) => {
+      reject(error);
+    });
+});
+
+export default { getAllHeadsFromDb, getAllTorsosFromDb, getAllLegsFromDb };
